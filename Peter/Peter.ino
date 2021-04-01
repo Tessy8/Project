d0dcc4740ca11176a5be93211b0ec86bfbb4b23b
Tessy <adamstestimony@gmail.com>
2021-04-01 17:07:03 +0100

    Added RTC and DHT parameters

--- Peter/Peter.ino
+++ Peter/Peter.ino
@@ -1,10 +1,29 @@
 // Code to select between two modes at the click of a button
-#define BUTTON 3
-int btnValue = 0;
+
+#include <Wire.h>
+#include <RTClib.h>
+#include <DHT.h>
+
+#define BUTTON 3        // Button Pin
+#define DHTPIN 2        // DHT Pin
+#define DHTTYPE DHT11   // DHT Type
+
+int btnValue;
+int previous = 0;
+int value = 0;
+
+// defining RTC and DHT parameter
+RTC_DS3231 rtc;
+DHT dht(DHTPIN, DHTTYPE);
+
+// DateTime now;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BUTTON, INPUT);
+
+  // Initialize RTC and DHT object
+  rtc.begin();
+  dht.begin();
 }
 
 // Temperature loop
@@ -20,8 +39,15 @@ void time_(){
 void loop() {
   // read input from button
   btnValue = digitalRead(BUTTON);
+  if (btnValue == 0 && previous == 0){
+    if (value == 1){
+      value = 0;
+    }else{
+      value = 1;
+    }
+  }
   
-  switch (btnValue) {
+  switch (value) {
     case 0:
       temp();
       break;
@@ -29,5 +55,5 @@ void loop() {
       time_();
       break;
 }
-  
+  previous == btnValue;
 }
