70754184161251d64d02b74fd3dd11da751c9a5e
Tessy <adamstestimony@gmail.com>
2021-04-03 07:33:16 +0100

    Code for Blynk App

---
+++ Blynk_/Blynk_.ino
@@ -0,0 +1,39 @@
+#define BLYNK_PRINT Serial
+#define PIN 3
+#define VIRTUALPIN V5
+#define BLYNK_MAX_SENDBYTES 1200
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+BLYNK_READ(VIRTUALPIN){
+  if (digitalRead(PIN) == 0){
+    Blynk.virtualWrite(VIRTUALPIN, "OFF");
+  }else if (digitalRead(PIN == 1){
+    Blynk.virtualWrite(VIRTUALPIN, "ON");
+  }
+}
+
+BLYNK_CONNECTED(){
+   Blynk.syncAll();  //to send the latest values of all values
+}
+
+void setup()
+{
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run(); 
+}
