40d2c20ff56e3c3a8dd1bbc973ff62d8e9193f17
Tessy <adamstestimony@gmail.com>
2021-04-06 14:22:51 +0100

    Added keypad functionality to code

--- Motor_Control/Motor_Control.ino
+++ Motor_Control/Motor_Control.ino
@@ -1,7 +1,28 @@
 // Code to rotate motor from 0 to 100% in 30 seconds in both directions
 
-#define FORWARDPIN 9
+#include <Keypad.h>
+
+#define FORWARDPIN 11
 #define REVERSEPIN 10
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+String num;                               // contains the input from keypad as a string
+float number;                             // contains the input from keypad as an float
+float motorIncrement;           
 
 void setup() {
   // Set pins as output
@@ -11,20 +32,38 @@ void setup() {
   // Turn off motor - Initial State
   digitalWrite(FORWARDPIN, LOW);
   digitalWrite(REVERSEPIN, LOW);
+
+  // Set maximum number of digit to be entered through keypad
+  num.reserve(10);
 }
 
 void loop() {
+  // Read input from keypad
+  char key = customKeypad.getKey();
+  if (key){
+    if (key >= '0' && key <= '9'){
+      num += key;                        // Add new character to string num
+    }else if (key == '#'){
+      if (num.length() > 0){
+        number = num.toInt();            // Convert string to integer
+        num = "";                        // Clear the input when # is pressed
+        motorIncrement = 255/number;     // value for incrementing motor rotation
+      }
+    }else if (key == '*'){
+      num = "";                       // Clear the input if a mistake is made when typing
+    }
+  }
+  
   // Forward
-  for (float motorValue = 0; motorValue <= 255; motorValue += 8.5){
+  for (float motorValue = 0; motorValue <= 255; motorValue += motorIncrement){
     digitalWrite(REVERSEPIN, LOW);
     analogWrite(FORWARDPIN, motorValue);
     delay(1000);
   }
   // Reverse
-  for (float motorValue = 0; motorValue <= 255; motorValue += 8.5){
+  for (float motorValue = 0; motorValue <= 255; motorValue += motorIncrement){
     digitalWrite(FORWARDPIN, LOW);
     analogWrite(REVERSEPIN, motorValue);
     delay(1000);
   }
-
 }
