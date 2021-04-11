af140ee344038e314114cbe01d46b4e8b47bd7e4
Tessy <adamstestimony@gmail.com>
2021-04-11 15:58:03 +0100

    Modified code for controlling servo to include 2 limit switches

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -5,11 +5,13 @@
 // Declare the pins
 #define SERVO_PIN 9
 #define PIN 3
-#define LIMIT_SWITCH 4
+#define LIMIT_SWITCH1 4
+#define LIMIT_SWITCH2 5
 
 // Define variables
 int pinValue;
-int swiValue;
+int swiValue1;
+int swiValue2;
 int previous1 = 0;
 int previous2 = 0;
 
@@ -21,30 +23,31 @@ void setup() {
    // We need to attach the servo to the used pin number 
    Servo1.attach(SERVO_PIN); 
    pinMode(PIN, INPUT);
-   pinMode(LIMIT_SWITCH, INPUT_PULLUP);
+   pinMode(LIMIT_SWITCH1, INPUT);
+   pinMode(LIMIT_SWITCH2, INPUT);
 }
 
 void loop() {
   pinValue = digitalRead(PIN);
   if (pinValue == HIGH){
     for (int i=0; i<=180; i+=15){
-      swiValue = digitalRead(LIMIT_SWITCH);
-      if (swiValue == 0 && previous1 == 0){
+      swiValue1 = digitalRead(LIMIT_SWITCH1);
+      if (swiValue1 == 0 && previous1 == 0){
         Servo1.write(i);
-        }else if (swiValue == 1 && previous1 == 0){
+        }else if (swiValue1 == 1 && previous1 == 0){
           break;
         }
-        previous1 == swiValue;
+        previous1 == swiValue1;
       }
   }else if (pinValue == LOW){
       for (int i=180; i>=0; i-=15){
-        swiValue = digitalRead(LIMIT_SWITCH);
-        if (swiValue == 0 && previous2 == 0){
+        swiValue2 = digitalRead(LIMIT_SWITCH2);
+        if (swiValue2 == 0 && previous2 == 0){
           Servo1.write(i);
-        }else if (swiValue == 1 && previous2 == 0){
+        }else if (swiValue2 == 1 && previous2 == 0){
           break;
         }
-        previous2 == swiValue;
+        previous2 == swiValue2;
       }
    }
 }
