5fc13b76066a83c5e04b4d6ca64cce81dd355c53
Tessy <adamstestimony@gmail.com>
2021-04-15 18:06:11 +0100

    Modified code for controlling servos with PCA9685 to include potentiometers

--- PCA9685_Servo/PCA9685_Servo.ino
+++ PCA9685_Servo/PCA9685_Servo.ino
@@ -5,31 +5,74 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();   // Initiates library.
 
 #define SERVOMIN  100  // Minimum pulse length count out of 4096.
 #define SERVOMAX  2200 // Maximum pulse length count out of 4096.
+#define FREQUENCY 60
 
-int servoNo = 0; // Defines a counter for servos.
+// Potentiometer pins
+#define POTPIN1 A0
+#define POTPIN2 A1
+#define POTPIN3 A2
+#define POTPIN4 A3
+#define POTPIN5 A4
+#define POTPIN6 A5
+
+// Servo pins
+#define servo1 3
+#define servo2 4
+#define servo3 5
+#define servo4 6
+#define servo5 7
+#define servo6 8
 
 
-void setup() 
-{
-  Serial.begin(9600);       // Starts serial connecton at 9600 baud rate.
+void setup() {
+  // Set potentiometers as input
+  pinMode(POTPIN1, INPUT);
+  pinMode(POTPIN2, INPUT);
+  pinMode(POTPIN3, INPUT);
+  pinMode(POTPIN4, INPUT);
+  pinMode(POTPIN5, INPUT);
+  pinMode(POTPIN6, INPUT);
   
   pwm.begin();         // Sends PWM signals.
-  pwm.setPWMFreq(60);  // Makes servos run at 60 Hz rate.
+  pwm.setPWMFreq(FREQUENCY);  // Makes servos run at 60 Hz rate.
   delay(20);
 }
 
 
-void loop() 
-{   
+void moveServo(int potIn, int servoOut)
+{
+  int pulse_wide, pulse_width, potVal;
   
-  for (int pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) // Drives each servo one at a time first                                                                                                
-    pwm.setPWM(servoNo, 0, pulselen);                            // to maximum pulse length then to minimum pulse length.
-  delay(300);
+  // Read values from potentiometer
+  potVal = analogRead(potIn);
   
-  for (int pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--)
-    pwm.setPWM(servoNo, 0, pulselen);
-  delay(300);
+  // Convert to pulse width
+  pulse_wide = map(potVal, 0, 1023, SERVOMIN, SERVOMAX);
+  pulse_width = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+  
+  //Control Motor
+  pwm.setPWM(servoOut, 0, pulse_width);
+ 
+}
+ 
+void loop() {
+ 
+  //Control First Servo
+  moveServo(POTPIN1, servo1);
 
-  servoNo ++;            // Proceeds to next servo.
-  if (servoNo > 6) servoNo = 0;
-} 
+  //Control Second Servo
+  moveServo(POTPIN2, servo2);
+
+  //Control Third Servo
+  moveServo(POTPIN3, servo3);
+
+  //Control Fourth Servo
+  moveServo(POTPIN4, servo4);
+
+  //Control Fifth Servo
+  moveServo(POTPIN5, servo5);
+
+  //Control Sixth Servo
+  moveServo(POTPIN6, servo6);
+  
+}
