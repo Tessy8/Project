ea4cda05321dcc40c3df96f7a28b45f63559fd03
Tessy <adamstestimony@gmail.com>
2022-01-11 18:45:47 +0100

    Modified code for Motor_Fault_Detection

---
+++ SCHEMATIC BLINKER/LED_Blinker/LED_Blinker.ino
@@ -0,0 +1,14 @@
+void setup() {
+  for (int pin = 2; pin < 14, pin++){
+    pinmode(pin, OUTPUT);
+  }
+  pinmode(A0, OUTPUT);
+  pinmode(A1, OUTPUT);
+  pinmode(A2, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
